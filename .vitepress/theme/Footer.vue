e784920b3470dce58fd7bc06d9e078b5da5aae75
Tim Harding <Tim@TimHarding.co>
2025-01-13 19:06:38 -0800

    Add feed link to footer

--- .vitepress/theme/Footer.vue
+++ .vitepress/theme/Footer.vue
@@ -8,6 +8,26 @@
       <nav :class="s.nav">
         <ul :class="s.list">
           <li>
+            <a :class="s.link" href="/blog.rss.xml">
+              <svg
+                :class="s.icon"
+                width="20px"
+                height="20px"
+                viewBox="0 0 16 16"
+              >
+                <g>
+                  <path
+                    d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"
+                  />
+                  <path
+                    d="M5.5 12a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0m-3-8.5a1 1 0 0 1 1-1c5.523 0 10 4.477 10 10a1 1 0 1 1-2 0a8 8 0 0 0-8-8a1 1 0 0 1-1-1m0 4a1 1 0 0 1 1-1a6 6 0 0 1 6 6a1 1 0 1 1-2 0a4 4 0 0 0-4-4a1 1 0 0 1-1-1"
+                  />
+                </g>
+              </svg>
+            </a>
+          </li>
+
+          <li>
             <a :class="s.link" href="mailto:tim@timharding.co">
               <svg
                 :class="s.icon"
@@ -116,6 +136,7 @@
   grid-auto-flow: column;
   gap: 0.5rem;
   align-content: center;
+  align-items: center;
 }
 
 .list {
